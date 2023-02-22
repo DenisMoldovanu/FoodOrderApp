f663b21c10d29d7ede74aa232fea305c766b6b8c
Denis <93701844+DenisMoldovanu@users.noreply.github.com>
2023-02-22 14:25:00 +0200

    Create Input component

--- src/components/Meals/MealsItem/MealsItemForm.js
+++ src/components/Meals/MealsItem/MealsItemForm.js
@@ -1,15 +1,21 @@
 
 import classes from './MealsItemForm.module.css'
+import Input from '../../UI/Input';
 
 const MealsItemForm = (props) => {
-
-    const price = `$ ${props.price.toFixed(2)}`;
+ 
 
     return(
     <form className={classes.form}>
-        <input type="text" />
+        <Input label="Amount" input={{
+            id: 'amount_' + props.id,
+            type: 'number',
+            min:'1',
+            max: '5',
+            step: '1',
+            defaultValue: '1'
+        }}  />
         <button>+ Add</button>
-      
     </form>
     )
 }
